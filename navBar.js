4a464e6ec2aa1f3f178add1eda3bb868953e2c6b
ChadKatzen <49657992+ChadKatzen@users.noreply.github.com>
2022-10-09 09:38:21 -0400

    Add files via upload

---
+++ navBar.js
@@ -0,0 +1,37 @@
+
+let $connectWallet = $('#connectWallet');
+
+
+function doSomething(){
+    window.alert("vagina");
+}
+function doSomethingElse(){
+  window.alert("penis");
+}
+
+
+
+$connectWallet.on('click', doSomething)
+
+
+$connectWallet.on('mouseover', function() {
+
+    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+    if (typeof web3 !== 'undefined') {
+      // Use Mist/MetaMask's provider
+      web3js = new Web3(web3.currentProvider);
+      doSomething();
+    } else {
+      // Handle the case where the user doesn't have Metamask installed
+      // Probably show them a message prompting them to install Metamask
+      doSomethingElse();
+    }
+
+    // Now you can start your app & access web3 freely:
+    //startApp()
+
+  })
+  
+
+
+
