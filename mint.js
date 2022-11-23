6a3c0b58418d47999963fd4bec85d65e9f5c1fbb
Chad Katzen <katzen.chad@gmail.com>
2022-11-23 13:35:01 -0500

    troubleshooting

--- mint.js
+++ mint.js
@@ -1,9 +1,10 @@
 async function mint(){
+    let debugMe =1;
     await NFTcontract.methods.mintTo(activeAccount).send({ from: activeAccount, value: web3js.utils.toWei("0.08", "ether") })
     $('#success').show();
-    
+
 }
 
-$mintButton=$("#mint");
+$mintButton=$('#mint');
 
 $mintButton.on('click', mint)
